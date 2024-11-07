63997ee4be06a3b1c8b249018a7d77010369f341
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-11-07 12:23:45 +0200

    upd

--- src/js/fetchProdactsAPI.js
+++ src/js/fetchProdactsAPI.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import axios from "axios";
+import { Loading } from "notiflix/build/notiflix-loading-aio";
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { MyLibrary } from './localStorage';
-import { refs } from './refs';
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import { MyLibrary } from "./localStorage";
+import { refs } from "./refs";
 
-const KEY = '32432509d17cea42104bbb7507a382c7';
+const KEY = "1c371b02c7ab52709deaac4809871490";
 const api_key = `?api_key=${KEY}`;
-const BASE_URL = 'https://api.themoviedb.org/3/';
+const BASE_URL = "https://api.themoviedb.org/3/";
 
 const myLibrary = new MyLibrary();
 export default class ApiService {
-  currentLang = localStorage.getItem('language');
+  currentLang = localStorage.getItem("language");
   constructor() {
     this.totalResults = 0;
-    this.searchQuery = '';
+    this.searchQuery = "";
     this.page = 1;
     this.filmsOnPage = 12;
     this.currentLang;
@@ -23,14 +23,14 @@ export default class ApiService {
   }
   //  фільми з більшим доходом
   async getRevenueFilms() {
-    if (this.currentLang === 'en') {
-      this.lang = 'en';
-    } else if (this.currentLang === 'ua') {
-      this.lang = 'uk';
+    if (this.currentLang === "en") {
+      this.lang = "en";
+    } else if (this.currentLang === "ua") {
+      this.lang = "uk";
     }
     try {
       const url = `${BASE_URL}discover/movie${api_key}&page=1&append_to_response=images&language=${this.lang}&sort_by=revenue.desc`;
-      return await axios.get(url).then(response => {
+      return await axios.get(url).then((response) => {
         if (!response) {
           throw new Error(response.status);
         }
@@ -44,18 +44,18 @@ export default class ApiService {
   // фільми топові
   async getPopularFilms() {
     try {
-      if (this.currentLang === 'en') {
-        this.lang = 'en';
-      } else if (this.currentLang === 'ua') {
-        this.lang = 'uk';
+      if (this.currentLang === "en") {
+        this.lang = "en";
+      } else if (this.currentLang === "ua") {
+        this.lang = "uk";
       }
-      Loading.pulse('Loading...', {
+      Loading.pulse("Loading...", {
         clickToClose: true,
-        svgColor: 'rgba(255, 107, 1, 0.6)',
-        backgroundColor: 'rgba(0,0,0,0.8)',
+        svgColor: "rgba(255, 107, 1, 0.6)",
+        backgroundColor: "rgba(0,0,0,0.8)",
       });
       const url = `${BASE_URL}trending/movie/week${api_key}&page=${this.page}&language=${this.lang}&append_to_response=images&sort_by = popularity.desc`;
-      return await axios.get(url).then(response => {
+      return await axios.get(url).then((response) => {
         if (!response) {
           throw new Error(response.status);
         }
@@ -71,18 +71,18 @@ export default class ApiService {
   // фільми, що шукають за назвою
   async getSearchFilms() {
     try {
-      if (this.currentLang === 'en') {
-        this.lang = 'en';
-      } else if (this.currentLang === 'ua') {
-        this.lang = 'uk';
+      if (this.currentLang === "en") {
+        this.lang = "en";
+      } else if (this.currentLang === "ua") {
+        this.lang = "uk";
       }
-      Loading.pulse('Loading...', {
+      Loading.pulse("Loading...", {
         clickToClose: true,
-        svgColor: 'rgba(255, 107, 1, 0.6)',
-        backgroundColor: 'rgba(0,0,0,0.8)',
+        svgColor: "rgba(255, 107, 1, 0.6)",
+        backgroundColor: "rgba(0,0,0,0.8)",
       });
       const url = `${BASE_URL}search/movie${api_key}&query=${this.searchQuery}&page=${this.page}&language=${this.lang}`;
-      return await axios.get(url).then(response => {
+      return await axios.get(url).then((response) => {
         if (!response) {
           throw new Error(response.status);
         }
@@ -96,18 +96,18 @@ export default class ApiService {
   // пошук фільму по id
   async getFilmById(id) {
     try {
-      if (this.currentLang === 'en') {
-        this.lang = 'en';
-      } else if (this.currentLang === 'ua') {
-        this.lang = 'uk';
+      if (this.currentLang === "en") {
+        this.lang = "en";
+      } else if (this.currentLang === "ua") {
+        this.lang = "uk";
       }
-      Loading.pulse('Loading...', {
+      Loading.pulse("Loading...", {
         clickToClose: true,
-        svgColor: 'rgba(255, 107, 1, 0.6)',
-        backgroundColor: 'rgba(0,0,0,0.8)',
+        svgColor: "rgba(255, 107, 1, 0.6)",
+        backgroundColor: "rgba(0,0,0,0.8)",
       });
       const url = `${BASE_URL}movie/${id}${api_key}&append_to_response=images&language=${this.lang}`;
-      return await axios.get(url).then(response => {
+      return await axios.get(url).then((response) => {
         if (!response) {
           throw new Error(response.status);
         }
@@ -121,20 +121,20 @@ export default class ApiService {
   //  пошук фільму по масиву id з localStorage
   async getFilmFromLocalStorage(arrayFilms) {
     const data = await Promise.all(
-      arrayFilms.map(idWatchedFilm => {
+      arrayFilms.map((idWatchedFilm) => {
         try {
-          if (this.currentLang === 'en') {
-            this.lang = 'en';
-          } else if (this.currentLang === 'ua') {
-            this.lang = 'uk';
+          if (this.currentLang === "en") {
+            this.lang = "en";
+          } else if (this.currentLang === "ua") {
+            this.lang = "uk";
           }
-          Loading.pulse('Loading...', {
+          Loading.pulse("Loading...", {
             clickToClose: true,
-            svgColor: 'rgba(255, 107, 1, 0.6)',
-            backgroundColor: 'rgba(0,0,0,0.8)',
+            svgColor: "rgba(255, 107, 1, 0.6)",
+            backgroundColor: "rgba(0,0,0,0.8)",
           });
           const url = `${BASE_URL}movie/${idWatchedFilm}${api_key}&append_to_response=images&page=${this.page}&language=${this.lang}`;
-          return axios.get(url).then(response => {
+          return axios.get(url).then((response) => {
             if (!response) {
               throw new Error(response.status);
             }
@@ -154,7 +154,7 @@ export default class ApiService {
     let arrWatchedFilms = myLibrary.getFromWatched();
     let filmsOnPage = [];
     if (arrWatchedFilms !== null) {
-      refs.libraryEmpty.classList.add('is-hidden');
+      refs.libraryEmpty.classList.add("is-hidden");
       if (this.page === 1) {
         filmsOnPage = arrWatchedFilms.slice(
           2 * (this.page - 1),
@@ -175,7 +175,7 @@ export default class ApiService {
     let arrQueueFilms = myLibrary.getFromQueue();
     let filmsOnPage = [];
     if (arrQueueFilms !== null) {
-      refs.libraryEmpty.classList.add('is-hidden');
+      refs.libraryEmpty.classList.add("is-hidden");
       if (this.page === 1) {
         filmsOnPage = arrQueueFilms.slice(
           2 * (this.page - 1),
--- src/js/functionsForFilms.js
+++ src/js/functionsForFilms.js
@@ -1,14 +1,14 @@
-import { refs } from './refs';
-import { onSearchFormSubmit } from './searchFilms';
-import ApiService from './fetchProdactsAPI';
-import { renderFilmCard } from './renderFunction';
-import { pagination } from './pagination';
+import { refs } from "./refs";
+import { onSearchFormSubmit } from "./searchFilms";
+import ApiService from "./fetchProdactsAPI";
+import { renderFilmCard } from "./renderFunction";
+import { pagination } from "./pagination";
 const apiService = new ApiService();
 // екземпляр класу в який пишемо виклики фільмів по потребі
 export function ShowFilms() {
   topFilms();
-  if (apiService.query !== '') {
-    refs.searchForm.getNewFilms('submit', onSearchFormSubmit);
+  if (apiService.query !== "") {
+    refs.searchForm.getNewFilms("submit", onSearchFormSubmit);
     return;
   }
 }
